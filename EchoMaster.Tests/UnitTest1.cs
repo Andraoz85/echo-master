f7d2493d820ca82641ccb9ee484885330a17d78b
Andraoz85 <andraoz85@gmail.com>
2024-01-28 23:02:06 +0100

    make test fail

--- EchoMaster.Tests/UnitTest1.cs
+++ EchoMaster.Tests/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace EchoMaster.Tests
         {
             string expected = "Hello World!";
 
-            string result = "Hello World!";
+            string result = "Hello";
 
             result.Should().BeEquivalentTo(expected);
         }
